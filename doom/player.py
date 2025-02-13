358d09997ec7359bea014bc015d8f6fb1a1a5a3a
Ben Hampton <BenHampton11@yahoo.com>
2025-02-13 12:13:08 -0600

    doom -> added start of raycasting

--- doom/player.py
+++ doom/player.py
@@ -55,11 +55,11 @@ class Player:
 
     def draw(self):
         # test player, test his direction of movement as a line an the player as a circle
-        pg.draw.line(self.game.screen,
-                     'yellow',
-                     (self.x * 100, self.y * 100),
-                     (self.x * 100 + WIDTH * math.cos(self.angle), self.y * 100 + WIDTH * math.sin(self.angle)),
-                     2)
+        # pg.draw.line(self.game.screen,
+        #              'yellow',
+        #              (self.x * 100, self.y * 100),
+        #              (self.x * 100 + WIDTH * math.cos(self.angle), self.y * 100 + WIDTH * math.sin(self.angle)),
+        #              2)
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
 
     def update(self):
