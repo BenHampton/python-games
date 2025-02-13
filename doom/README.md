c8025f09a07fa281689bf79ee62896b7b92e8d0d
Ben Hampton <BenHampton11@yahoo.com>
2025-02-13 14:56:00 -0600

    doom -> added texture to map walls and fix *.png files

--- doom/README.md
+++ doom/README.md
@@ -8,3 +8,11 @@
  ----------------  =  ----------------  =>  PROJECTED_HEIGHT  =  -----------------
    SCREEN_DIST             DEPTH                                       DEPTH
 ```
+## Notes
+- needed to use `pngcrush` to fix *.png files
+  - install via homebrew: `brew install pngcrush` 
+  - use command: `pngcrush -ow -rem allb -reduce file.png`
+
+## Reference
+#### youtube link: https://www.youtube.com/watch?v=ECqUrT7IdqQ&list=PLi77irUVkDatlbulEY4Kz8O107HO8RGH8
+#### github link: https://github.com/StanislavPetrovV/DOOM-style-Game
--- doom/raycasting.py
+++ doom/raycasting.py
@@ -10,11 +10,45 @@ from settings import *
 class RayCasting:
     def __init__(self, game):
         self.game = game
+        self.ray_casting_result = []
+        self.objects_to_render = []
+        self.textures = self.game.object_renderer.wall_textures
 
-    # yt tag to explain the math - 9:40
+    def get_objects_to_render(self):
+        self.objects_to_render = []
+        for ray, values in enumerate(self.ray_casting_result):
+            depth, proj_height, texture, offset = values
+
+            # determine how to scale texture to avoid resource intensive actions
+            if proj_height < HEIGHT:
+                wall_column = self.textures[texture].subsurface(
+                    offset * (TEXTURE_SIZE - SCALE),
+                    0,
+                    SCALE,
+                    TEXTURE_SIZE
+                )
+                wall_column = pg.transform.scale(wall_column, (SCALE, proj_height))
+                wall_pos = (ray * SCALE, HALF_HEIGHT - proj_height // 2)
+            else:
+                texture_height = TEXTURE_SIZE * HEIGHT / proj_height
+                wall_column = self.textures[texture].subsurface(
+                    offset * (TEXTURE_SIZE - SCALE),
+                    HALF_TEXTURE_SIZE - texture_height // 2,
+                    SCALE,
+                    texture_height
+                )
+                wall_column = pg.transform.scale(wall_column, (SCALE, HEIGHT))
+                wall_pos = (ray * SCALE, 0)
+
+            self.objects_to_render.append((depth, wall_column, wall_pos))
+
+    # yt tag to explain the math for ray casting - 9:40
     def ray_cast(self):
+        self.ray_casting_result = []
         ox, oy = self.game.player.pos # player coords
         x_map, y_map = self.game.player.map_pos # coors of players tile
+
+        texture_vert, texture_hor = 1, 1
 
         ray_angle = self.game.player.angle - HALF_FOV + 0.0001
         for ray in range(NUM_RAYS):
@@ -33,6 +67,7 @@ class RayCasting:
             for i in range(MAX_DEPTH):
                 tile_hor = int(x_hor), int(y_hor)
                 if tile_hor in self.game.map.world_map:
+                    texture_hor = self.game.map.world_map[tile_hor]
                     break
                 x_hor += dx
                 y_hor += dy
@@ -50,29 +85,38 @@ class RayCasting:
             for i in range(MAX_DEPTH):
                 tile_vert = int(x_vert), int(y_vert)
                 if tile_vert in self.game.map.world_map:
+                    texture_vert = self.game.map.world_map[tile_vert]
                     break
                 x_vert += dx
                 y_vert += dy
                 depth_vert += delta_depth
 
-            # depth
+            # depth, texture offset
             if depth_vert < depth_hor:
-                depth = depth_vert
+                depth, texture = depth_vert, texture_vert
+                y_vert %= 1
+                offset = y_vert if cos_a > 0 else (1 - y_vert)
             else:
-                depth = depth_hor
+                depth, texture = depth_hor, texture_hor
+                x_hor %= 1
+                offset = (1 - x_hor) if sin_a > 0 else x_hor
 
             # remove fishbowl effect
             depth *= math.cos(self.game.player.angle - ray_angle)
 
             # projection
-            color = [255 / (1 + depth ** 5 * 0.0002)] * 3
             proj_height = SCREEN_DIST / (depth + 0.0001)
 
-            # draw walls
-            pg.draw.rect(self.game.screen,
-                         color,
-                         (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
+            # ray casting result
+            self.ray_casting_result.append((depth, proj_height, texture, offset))
 
+            # no longer needed with textures
+            # draw walls
+            # color = [255 / (1 + depth ** 5 * 0.00002)] * 3
+            # pg.draw.rect(self.game.screen,
+            #              color,
+            #              (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height))
+            #
             # draw for debugging
             # pg.draw.line(self.game.screen,
             #              'yellow',
@@ -84,3 +128,4 @@ class RayCasting:
 
     def update(self):
         self.ray_cast()
+        self.get_objects_to_render()
