755b0d12520be5dc7ff0f49922f7019f12458e11
Ben Hampton <BenHampton11@yahoo.com>
2025-02-17 20:04:20 -0600

    doom -> added init commit for hud

--- doom/hud.py
+++ doom/hud.py
@@ -40,68 +40,52 @@ class Hud:
         self.draw_inventory()
         self.draw_padding_blocks()
 
+    def draw_section(self, index):
+        return (self.hud_sections_start[index],
+                      FULL_HUB_HEIGHT,
+                      self.hud_sections_width[index],
+                      FULL_HUB_HEIGHT)
 
     def draw_padding_blocks(self):
         # # todo Rect((left, top), (width, height)) -> Rect
-        pg.draw.rect(self.game.screen, 'black',
-                     (self.hud_sections_start[0],
-                      FULL_HUB_HEIGHT,
-                      self.hud_sections_width[0],
-                      FULL_HUB_HEIGHT))
-        pg.draw.rect(self.game.screen, 'black',
-                     (self.hud_sections_start[7],
-                      FULL_HUB_HEIGHT,
-                      self.hud_sections_width[7],
-                      FULL_HUB_HEIGHT))
+        pg.draw.rect(self.game.screen, 'black', self.draw_section(0))
+        pg.draw.rect(self.game.screen, 'black', self.draw_section(7))
 
     def draw_ammo(self):
         pg.draw.rect(self.game.screen, 'green',
-                     (self.hud_sections_start[1],
-                      FULL_HUB_HEIGHT ,
-                      self.hud_sections_width[1],
-                      FULL_HUB_HEIGHT))
+                     self.draw_section(1))
 
     def draw_player_health(self):
-        pg.draw.rect(self.game.screen, 'white',
-                     (self.hud_sections_start[2],
-                      FULL_HUB_HEIGHT,
+        # pg.draw.rect(self.game.screen, 'white', self.draw_section(2))
+        health = str(self.game.player.health)
+        for i, char in enumerate(health):
+            self.screen.blit(self.digits[char], (i * self.digit_size + self.hud_sections_start[2] + 10,
+                      FULL_HUB_HEIGHT + 10,
                       self.hud_sections_width[2],
                       FULL_HUB_HEIGHT))
-        # health = str(self.game.player.health)
+        self.screen.blit(self.digits['10'], ((i + 1) * self.digit_size + self.hud_sections_start[2] + 5,
+                      FULL_HUB_HEIGHT + 10,
+                      self.hud_sections_width[2],
+                      FULL_HUB_HEIGHT))
         # for i, char in enumerate(health):
-        #     self.screen.blit(self.digits[char], (i * self.digit_size + 250, FULL_HUB_HEIGHT + 30))
-        # self.screen.blit(self.digits['10'], ((i + 1) * self.digit_size + 250, FULL_HUB_HEIGHT + 30))
+        #     self.screen.blit(self.digits[char], (i * self.digit_size + 250, self.hud_sections_width[2]))
+        # self.screen.blit(self.digits['10'], ((i + 1) * self.digit_size + 250, self.hud_sections_width[2]))
 
     def draw_number_pad(self):
-        pg.draw.rect(self.game.screen, 'yellow',
-                     (self.hud_sections_start[3],
-                      FULL_HUB_HEIGHT,
-                      self.hud_sections_width[3],
-                      FULL_HUB_HEIGHT))
+        pg.draw.rect(self.game.screen, 'yellow', self.draw_section(3))
 
     def draw_face(self):
-        pg.draw.rect(self.game.screen, 'blue',
-                     (self.hud_sections_start[4],
-                      FULL_HUB_HEIGHT,
-                      self.hud_sections_width[4],
-                      FULL_HUB_HEIGHT))
+        pg.draw.rect(self.game.screen, 'blue', self.draw_section(4))
 
     def draw_armor(self):
-        pg.draw.rect(self.game.screen, 'purple',
-                     (self.hud_sections_start[5],
-                      FULL_HUB_HEIGHT,
-                      self.hud_sections_width[5],
-                      FULL_HUB_HEIGHT))
+        pg.draw.rect(self.game.screen, 'purple', self.draw_section(5))
 
     def draw_inventory(self):
-        pg.draw.rect(self.game.screen, 'orange',
-                     (self.hud_sections_start[6],
-                      FULL_HUB_HEIGHT,
-                      self.hud_sections_width[6],
-                      FULL_HUB_HEIGHT))
+        pg.draw.rect(self.game.screen, 'orange', self.draw_section(6))
 
     def draw_background(self):
         self.screen.blit(self.hud_image, (0, self.hud_height))
+
 
 
 
