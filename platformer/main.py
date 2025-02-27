1903f26e35e133a6c627e9b484bcdb43eb3f70e7
Ben Hampton <BenHampton11@yahoo.com>
2025-02-27 12:55:45 -0600

    platformer -> added background

--- platformer/main.py
+++ platformer/main.py
@@ -9,15 +9,58 @@ pg.init()
 
 pg.display.set_caption("Platformer")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800
 FPS = int(60)
 PLAYER_VEL = 5
 
 window = pg.display.set_mode((WIDTH, HEIGHT))
 
+class Player(pg.sprite.Sprite):
+    COLOR = (255, 0, 0)
+
+    def __init__(self, x, y, width, height):
+        self.rect = pg.Rect(x, y, width, height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+        if self.direction != 'right':
+            self.direction = "right"
+            self.animation_count = 0
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != 'left':
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.y_vel = vel
+
+def get_background(name):
+    image = pg.image.load(join("assets", "Background", name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+    return tiles, image
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pg.display.update()
+
 def main(window):
     clock = pg.time.Clock()
+    background, bg_image = get_background("Blue.png")
 
     run = True
     while run:
@@ -28,6 +71,7 @@ def main(window):
                 run = False
                 break
 
+        draw(window, background, bg_image)
 
 if __name__ == "__main__":
     main(window)
