489773121cc4c06d3928c0b459c4caa0440fba3c
Ben <benhampton11@yahoo.com>
2025-03-04 23:01:17 -0600

    doom - added menu options

--- doom/main.py
+++ doom/main.py
@@ -1,5 +1,5 @@
 ﻿import sys
-
+from button import Button
 from ground_weapon import GroundWeapon
 from doom.hud import Hud
 from map import *
@@ -13,9 +13,11 @@ from sound import *
 from weapon import *
 from inventory import *
 
+SCREEN = pg.display.set_mode((WIDTH, FULL_HEIGHT))
+
 class Game:
     def __init__(self):
-        pg.init()
+        # pg.init()
         pg.mouse.set_visible(False)
 
         # todo find a better way to toggle 2D/3D mode
@@ -27,7 +29,7 @@ class Game:
 
         self.new_weapon = None
 
-        self.screen = pg.display.set_mode((WIDTH, FULL_HEIGHT))
+        self.screen = SCREEN #pg.display.set_mode((WIDTH, FULL_HEIGHT))
         self.map_level = 0
         self.clock = pg.time.Clock()
         self.delta_time = 1
@@ -108,8 +110,60 @@ class Game:
             self.update()
             self.draw()
 
+def get_font(size):  # Returns Press-Start-2P in the desired size
+    text_font = pg.font.Font('resources/font/doom.ttf', size)
+    return text_font
+
+def menu():
+    pg.display.set_caption("Menu")
+    bg = pg.image.load('resources/textures/background.png')
+
+    while True:
+
+        SCREEN.blit(bg, (0, 0))
+
+        MENU_MOUSE_POS = pg.mouse.get_pos()
+
+        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+
+        PLAY_BUTTON = Button(image=pg.image.load("resources/textures/play_rect.png"), pos=(640, 250),
+                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pg.image.load("resources/textures/options_rect.png"), pos=(640, 400),
+                                text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4",
+                                hovering_color="White")
+        QUIT_BUTTON = Button(image=pg.image.load("resources/textures/quit_rect.png"), pos=(640, 550),
+                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    game = Game()
+                    if not game.sound_disabled:
+                        game.sound.theme.play()
+                    game.run()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    print('options')
+                    # options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pg.quit()
+                    sys.exit()
+
+        pg.display.update()
+
 if __name__ == '__main__':
-    game = Game()
-    if not game.sound_disabled:
-        game.sound.theme.play()
-    game.run()
+    pg.init()
+    menu()
+    # game = Game()
+    # if not game.sound_disabled:
+    #     game.sound.theme.play()
+    # game.run()
